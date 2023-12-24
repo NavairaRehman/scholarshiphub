fb45dd12c7db1c23c6b0f5583155ce3b68352499
Navaira Rehman <nrehman.bese21seecs@student.nust.edu.pk>
2023-12-25 02:04:30 +0500

    Create and set up migration files and models files

---
+++ database/migrations/2023_12_24_204030_create_scholarships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scholarships', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedTinyInteger('eligibility_age')->nullable();
+            $table->string('qualification')->nullable();
+            $table->date('start_date');
+            $table->date('deadline');
+            $table->string('web_link'); 
+            $table->foreignId('country_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scholarships');
+    }
+};
